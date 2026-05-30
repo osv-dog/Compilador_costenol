2d36298141c52c8ebd38f4220141356d5b9bdc9a
ILTyrex <amorochocordoba@gmail.com>
2026-05-29 19:18:07 -0500

    Hola

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import sys
 import os
 
-# Asegura que la raíz del proyecto esté en el path,
-# necesario para que los imports de core/ y gui/ funcionen correctamente
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from gui.interfaz import iniciar_interfaz
